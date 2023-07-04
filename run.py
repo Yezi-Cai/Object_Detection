2385bf0bcad9d6f2fa54bc6a10cda254cd1ffa47
Christine Cai <113726179+Yezi-Cai@users.noreply.github.com>
2023-07-04 16:29:01 -0700

    Add video capture

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
+import numpy as np
 import cv2 as cv
 
-print("opencv version:",cv.__version__)
+cap = cv.VideoCapture(0)  # 0 is the default camera
+# Check if the camera is opened successfully
+if not cap.isOpened():
+  print("Cannot open camera")
+  exit()
+
+# Start a loop to continuously capture frames from the camera
+while True:
+  # Capture frame-by-frame
+  ret, frame = cap.read() # ret is true if the frame is available
+                          # frame is an image array vector
+  # Check if the frame was captured successfully
+  if not ret:
+    print("Can't receive frame (stream end?). Exiting ...")
+    break
+
+  # Display the resulting frame
+  cv.imshow('frame', frame)  #imshow(<window name>, <image data>)
+
+  # Exit the loop if 'q' is pressed
+  if cv.waitKey(1) == ord('q'):  # waitKey(1) displays frame for 1ms
+    break                        # ord('q') returns the Unicode code point of 'q'
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
